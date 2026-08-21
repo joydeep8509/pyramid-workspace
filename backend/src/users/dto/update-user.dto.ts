9464c5e22b5e4fc3ba42588d525a0999eed7ca79
Joydeep Bhandari <joydeepbhandari76@gmail.com>
2026-08-21 13:49:18 +0530

    Refactor update-user.dto.ts import statement

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,5 +1,4 @@
-// src/users/dto/update-user.dto.ts
-import { PartialType } from '@nestjs/mapped-types'; // You may need to run: npm i @nestjs/mapped-types
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {}
