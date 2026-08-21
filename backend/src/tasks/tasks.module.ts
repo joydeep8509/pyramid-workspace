a13939d4b5abc391d27d478b1a79180fedd6e356
Joydeep Bhandari <joydeepbhandari76@gmail.com>
2026-08-21 13:48:48 +0530

    Update tasks.module.ts

--- backend/src/tasks/tasks.module.ts
+++ backend/src/tasks/tasks.module.ts
@@ -1,11 +1,9 @@
-// backend/src/tasks/tasks.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksController } from './tasks.controller';
 import { Task } from './entities/task.entity';
 
 @Module({
-  // Register the Task entity so TypeORM can inject the repository
   imports: [TypeOrmModule.forFeature([Task])],
   controllers: [TasksController],
 })
